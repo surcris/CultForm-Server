43cdad0473d964ccfbe5e7d4e866c4be1e162ec2
surcris <sympta113@gmail.com>
2023-05-05 03:07:16 +0200

    package node

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app/dist/index.cjs.js"
+import 'firebase/database/dist/index.cjs.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: "game-e6fea",
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const dbJoueur = firebase.database().ref('joueur');
+
+
+export default dbJoueur;
