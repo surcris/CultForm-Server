b4913bec93b88c9335f326a383b19e31585b59f9
surcris <sympta113@gmail.com>
2023-06-14 10:36:52 +0200

    add verif personnage

--- Routes/Personnages.js
+++ Routes/Personnages.js
@@ -6,15 +6,30 @@ const routerPers = express.Router();
 
 routerPers.put('/pushPerso/', (req, res) => {
     //const id = req.params.id;
-    const personnageData = req.body;
-
-    dbJoueur.push(personnageData, (error) => {
-        if (error) {
-            res.status(500).json({ message: 'Erreur lors de l\'enregistrement du personnage' });
-        } else {
-            res.status(200).json({ message: 'Personnage enregistrer avec succée' });
-        }
-    });
+    try {
+        const personnageData = req.body;
+        const idToken = req.headers['authorization'];
+        jwt.verify(idToken, jwtSecret, async (err, decodedToken) => {
+            if (err) {
+                // Le token est invalide ou a expiré
+                console.log("Unauthorized pushPerso");
+                res.status(401).json({ message: 'Unauthorized' });
+            } else {
+                console.log("Authorisation pour créer un personnage");
+                dbJoueur.push(personnageData, (error) => {
+                    if (error) {
+                        res.status(500).json({ message: 'Erreur lors de l\'enregistrement du personnage' });
+                    } else {
+                        console.log("Personnage enregistrer avec succée");
+                        res.status(200).json({ message: 'Personnage enregistrer avec succée' });
+                    }
+                });
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving data');
+    }
 });
 
 routerPers.put('/api/data/:id', (req, res) => {
@@ -40,8 +55,10 @@ routerPers.put("/search/id/", async (req, res) => {
         jwt.verify(idToken, jwtSecret, async (err, decodedToken) => {
             if (err) {
                 // Le token est invalide ou a expiré
+                console.log("Unauthorized search ID");
                 res.status(401).json({ message: 'Unauthorized' });
             } else {
+                console.log("Authorisation pour rechercher des personnage avec un ID");
                 //console.log(decodedToken);
                 const uid = decodedToken.uid;
                 const snapshot = await dbJoueur.once('value');
@@ -78,37 +95,49 @@ routerPers.put("/search/id/", async (req, res) => {
     }
 });
 
-routerPers.get("/search/pseudo/:pseudo", async (req, res) => {
-    const pseudo = req.params.pseudo;
+routerPers.put("/search/pseudo/", async (req, res) => {
+    const pseudo = req.headers['pseudo'];
     const persoData = [];
-
+    //console.log(pseudo)
     try {
-        const snapshot = await dbJoueur.once('value');
-        const data = snapshot.val();
+        const idToken = req.headers['authorization'];
+        jwt.verify(idToken, jwtSecret, async (err, decodedToken) => {
+            if (err) {
+                // Le token est invalide ou a expiré
+                console.log("Unauthorized search pseudo");
+                res.status(401).json({ message: 'Unauthorized' });
+            } else {
+                console.log("Authorisation pour rechercher un personnage avec un pseudo");
+                const snapshot = await dbJoueur.once('value');
+                const data = snapshot.val();
 
-        if (data) {
-            if (Array.isArray(data)) {
-                data.forEach(item => {
-                    if (item.pseudo === pseudo) {
-                        persoData.push(item);
-                    }
-                });
-            } else if (typeof data === 'object') {
-                for (const key in data) {
-                    if (data.hasOwnProperty(key) && data[key].pseudo === pseudo) {
-                        persoData.push(data[key]);
+                if (data) {
+                    if (Array.isArray(data)) {
+                        data.forEach(item => {
+                            if (item.pseudo === pseudo) {
+                                persoData.push(item);
+                            }
+                        });
+                    } else if (typeof data === 'object') {
+                        for (const key in data) {
+                            if (data.hasOwnProperty(key) && data[key].pseudo === pseudo) {
+                                persoData.push(data[key]);
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        if (persoData.length > 0) {
-            const jsonString = JSON.stringify(persoData);
-            res.status(200).json({ message: 'Personnage trouver avec ce Pseudo!' });
-            //res.json(jsonString);
-        } else {
-            res.status(404).json({ message: 'Aucune donnée trouvée pour ce Pseudo.' });
-        }
+                if (persoData.length > 0) {
+                    const jsonString = JSON.stringify(persoData);
+                    console.log("Personnage trouver avec ce Pseudo!");
+                    res.status(200).json({ message: 'Personnage trouver avec ce Pseudo!' });
+                    //res.json(jsonString);
+                } else {
+                    console.log("Personnage trouver avec ce Pseudo!");
+                    res.status(404).json({ message: 'Aucune donnée trouvée pour ce Pseudo.' });
+                }
+            }
+        });
     } catch (error) {
         console.error(error);
         res.status(500).send('Error retrieving data');
