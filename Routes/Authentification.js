6f98e3d8d0e51c4a390d992b217fe70a8bb879ed
Niel Abdallah <sympta113@gmail.com>
2024-06-05 18:37:59 +0200

    AUTH

--- Routes/Authentification.js
+++ Routes/Authentification.js
@@ -70,32 +70,107 @@ routerAuth.put("/deconnexion", async (req, res) => {
 });
 
 routerAuth.put("/connexionUser", async (req, res) => {
-  const infoConnexion = req.body;
-  const clientIPReceivedIP = req.connection.remoteAddress;
+
+  // const clientIPReceivedIP = req.connection.remoteAddress;
 
   //Commence après le dernier (:)
-  const startIndex = clientIPReceivedIP.lastIndexOf(":") + 1;
+  // const startIndex = clientIPReceivedIP.lastIndexOf(":") + 1;
   //extraire startIndex de tout la chaine de charactère
-  const ipAddressWithoutPrefix = clientIPReceivedIP.substring(startIndex);
-  const clientIP = ipAddressWithoutPrefix;
-  
+  // const ipAddressWithoutPrefix = clientIPReceivedIP.substring(startIndex);
+  // const clientIP = ipAddressWithoutPrefix;
+  try {
+    const infoConnexion = req.body;
+    const em = decrypt(infoConnexion.email)
+    const ps = decrypt(infoConnexion.password)
 
-  auth.signInWithEmailAndPassword(infoConnexion.email, infoConnexion.password)
-    .then((userCredential) => {
-      // Connexion réussie, récupérer l'utilisateur connecté
-      const user = userCredential.user;
-      console.log('Utilisateur connecté ' );
-      const userSend = auth.currentUser;
-      const clientInfo = {uip:userSend.uid,ip:clientIP}
+    const aut = await auth.signInWithEmailAndPassword(em, ps)
+    const userSend = auth.currentUser;
+    console.log('Utilisateur connecté');
+    res.status(200).json({ message: encryptData(userSend.uid) });
+  } catch (error) {
+    console.error('Erreur de connexion :', error);
+    if (error.code === 'auth/invalid-email' || error.code === 'auth/wrong-password' || error.code === 'auth/user-not-found') {
+      // L'adresse e-mail ou le mot de passe est invalide
+      res.status(400).send({ message: "Erreur d'identifiant." });
+      console.log("Erreur d'identifiant.");
+    } else {
+      // Autres erreurs
+      console.log('Erreur inattendue:', error.message);
+      res.status(500).send({ message: 'Erreur inattendue.' });
+    }
+  }
 
-      res.status(200).json({ message: encryptData(userSend.uid)  });
-      //console.log(userCredential)
-    })
-    .catch((error) => {
-      // Erreur lors de la connexion, gérer l'erreur
-      console.error('Erreur de connexion :', error);
-    });
 
 });
+
+routerAuth.put("/inscripUser", async (req, res) => {
+  try {
+    const infoUser = req.body;
+    const pseudo = decrypt(infoUser.pseudo)
+    const email = decrypt(infoUser.email)
+    const password = decrypt(infoUser.password)
+
+    // Vérifiez si l'utilisateur existe déjà
+    try {
+      const userRecord = await authe.getUserByEmail(email);
+      // console.log(`Utilisateur existant: ${userRecord.uid}`);
+      console.log(`Utilisateur existant`);
+      res.status(400).send({ message: 'Utilisateur existant' });
+    } catch (error) {
+      console.log('Aucun compte à cette adresse email, création en cours...');
+
+      // Créez un nouvel utilisateur
+      const userRecord = await authe.createUser({
+        email: email,
+        password: password,
+        displayName: pseudo
+      });
+      console.error('Utilisateur créer avec succée:');
+      // res.status(201).send({ message: 'Utilisateur créer avec succée' });
+
+      // Connectez l'utilisateur créé
+      try {
+        // Connecter l'utilisateur après création
+        const userCredential = await auth.signInWithEmailAndPassword(email, password);
+        const user = userCredential.user;
+        console.log('Utilisateur connecté');
+        const userSend = auth.currentUser;
+
+        return res.status(200).json({ message: encryptData(userSend.uid) });
+      } catch (signInError) {
+        console.error('Erreur lors de la connexion :', signInError);
+        return res.status(500).send({ message: 'Erreur lors de la connexion', error: signInError.message });
+      }
+    }
+
+    // res.status(201).send({ message: 'User created successfully', user: userRecord });
+  } catch (error) {
+    console.error('Erreur lors de la création:', error);
+    res.status(500).send({ message: 'Erreur lors de la création', error: error.message });
+  }
+});
+
+
+routerAuth.put('/resetmdp',async (req, res) => {
+
+  //confirmPasswordReset(code :  string ,  newPassword :  string)
+
+  try {
+    const infoUser = req.body;
+    const email = decrypt(infoUser.email)
+    console.log(infoUser)
+    const rez = await auth.sendPasswordResetEmail(infoUser.email)
+
+    return res.status(200).json({ message: "Un e-mail de réinitialisation de mot de passe a été envoyé à votre adresse e-mail." });
+  } catch (error) {
+    if (error.code === "auth/user-not-found") {
+      console.error('Aucun compte à cette adresse email :', error);
+      return res.status(400).send({ message: 'Aucun compte à cette adresse email', error: error.message });
+    } else {
+      console.error('Erreur inattendue :', error);
+      return res.status(500).send({ message: 'Erreur inattendue', error: error.message });
+    }
+  }
+})
 
 export default routerAuth;
